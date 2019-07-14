586a6722ce3e2dd0aef456ad02b8fa51a3fdd9f0
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-07-14 09:51:51 -0700

    coded application.js funcs

--- app/components/Application.js
+++ app/components/Application.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Application extends Component {
-  state = {
-    items: [{ value: 'Pants', id: Date.now(), packed: false }]
+const Application = () => {
+  const [items, setItems] = useState([
+    { value: 'Pants', id: Date.now(), packed: false }
+  ]);
+
+  const filterItems = () => {
+    const packed = items.map(item => item.packed);
+    const unPacked = items.map(item => !item.packed);
+    return { packed, unPacked };
   };
 
-  render() {
-    console.log(this.state);
-    return (
-      <div>
-        <h1>Hello World!</h1>
-        <button className="full-width">This button does not do anything.</button>
-      </div>
-    );
-  }
+  const addItem = item => setItems([...items, item]);
+
+  const markAsPacked = item => {};
+
+  const markAllAsUnPacked = () => {
+    const updatedItems = [...items].map(item => {
+      item.packed = false;
+      return item;
+    });
+    setItems(updatedItems);
+  };
+
+  const { packed, unPacked } = filterItems();
+
+  return (
+    <div className="application">
+      {/* TO BE IMPLEMENTED: <NewItem /> */}
+      {/* TO BE IMPLEMENTED: <Items title="Unpacked Items" /> */}
+      {/* TO BE IMPLEMENTED: <Items title="Packed Items" /> */}
+      <button className="full-width" onClick={markAllAsUnPacked}>Mark All Unpacked</button>
+    </div>
+  );
 };
 
 export default Application;
