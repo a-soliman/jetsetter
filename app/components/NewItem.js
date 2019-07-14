82ba961f8ab492b726087e31ed8652eb16604632
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-07-14 10:41:21 -0700

    coded newItem form component

---
+++ app/components/NewItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const NewItem = ({onSubmit}) => {
+  const [value, setValue ] = useState('');
+
+  const handleChange = evt => {
+    setValue(evt.target.value);
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    if (value === '') return;
+    const newItem = { value, id: Date.now(), packed:false };
+    onSubmit(newItem);
+    setValue('');
+  }
+
+  return (
+    <form className="NewItem" onSubmit={handleSubmit}>
+      <input type="text" value={value} className="NewItem-input" onChange={handleChange} />
+      <input type="submit" className="NewItem-submit button" disabled={!value.length} />
+    </form>
+  );
+};
+
+export default NewItem;
