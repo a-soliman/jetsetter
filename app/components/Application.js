df2d7da42c3673c30c9ef076065174caaf6cd526
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-07-16 07:51:26 -0700

    coded and wired rdeleteUnpackedItems

--- app/components/Application.js
+++ app/components/Application.js
@@ -59,14 +59,35 @@ const Application = ({ database }) => {
     }
   };
 
+  const deleteItem = async (id) => {
+    try {
+      await database('items').where('id', id).delete();
+      await fetchItems();
+    }
+    catch(e) {
+      console.error(e);
+    }
+  };
+
+  const deleteUnPackedItems = async () => {
+    try {
+      await database('items').where('packed', false).delete();
+      await fetchItems();
+    }
+    catch(e) {
+      console.error(e);
+    }
+  };
+
   const { packed, unPacked } = filterItems();
 
   return (
     <div className="application">
       <NewItem onSubmit={addItem} />
-      <Items title="Unpacked Items" items={unPacked} onCheckOff={togglePacked} />
-      <Items title="Packed Items" items={packed} onCheckOff={togglePacked} />
+      <Items title="Unpacked Items" items={unPacked} onCheckOff={togglePacked} onDelete={deleteItem} />
+      <Items title="Packed Items" items={packed} onCheckOff={togglePacked} onDelete={deleteItem} />
       <button className="full-width" onClick={markAllAsUnPacked}>Mark All Unpacked</button>
+      <button className="button full-width-secondary" onClick={deleteUnPackedItems}>Remove Unpacked Items</button>
     </div>
   );
 };
