53d991163666582dc2703d99c3ff49a4a4204009
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-07-16 07:22:41 -0700

    downgraded versions to get the correct sqlite3 build for electron

--- app/database.js
+++ app/database.js
@@ -1,26 +1,23 @@
-//const sqlite3 = require('sqlite3');
 import 'sqlite3' ;
-//import 'knex';
-//import 'sqlite3';
-//const knex = require('knex');
-// const database = Knex({
-//   client: 'sqlite3',
-//   connection: {
-//     filename: './db.sqlite'
-//   },
-//   useNullAsDefault: true
-// });
+import knex from 'knex';
 
-// database.schema.hasTable('items').then(exists => {
-//   if(!exists) {
-//     return database.schema.createTable('items', t => {
-//       t.increments('id').primary();
-//       t.string('value', 100);
-//       t.boolean('packed');
-//     });
-//   }
-// })
+const database = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './db.sqlite'
+  },
+  useNullAsDefault: true
+});
+
+database.schema.hasTable('items').then(exists => {
+  if(!exists) {
+    return database.schema.createTable('items', t => {
+      t.increments('id').primary();
+      t.string('value', 100);
+      t.boolean('packed');
+    });
+  }
+})
 
 
-const database = {};
 export default database;
